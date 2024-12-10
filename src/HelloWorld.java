42ec4f3e7394e3633fa790487c45695253f80ede
boldoo80 <batmunkh1107@gmail.com>
2024-12-10 10:17:42 -0600

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello baby");
+        System.out.println("hello honey");
+        System.out.println("hello mama");
     }
 }
