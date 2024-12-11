4b20ff8d7853e416e2b7fe50107a09dd6e4baeaf
batmunkhboldbaatar <batmunkh1107@gmail.com>
2024-12-11 08:45:36 -0600

    HelloCydeo updated

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -1,7 +1,9 @@
+package day1;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello baby");
         System.out.println("hello honey");
-        System.out.println("hello mama");
+        System.out.println("hello baby");
     }
 }
